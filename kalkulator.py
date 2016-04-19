7216ef17ba7f9e5ca77713a9c7471adc679263aa
czajkowskir <czajkowski.rafal.92@gmail.com>
2016-04-19 15:22:25 +0200

    Dodano plik kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,22 @@
+def wlasne_csv(separator,lokalizacja):
+    count = -1
+    for count, wiersz in enumerate(open(lokalizacja, 'rU')):
+        pass
+    count += 1
+    plik_csv = open(lokalizacja, 'r')
+    lista = []
+    for i in range(0, count):
+        zaczytanie_linii = plik_csv.readline()
+        lista.append(zaczytanie_linii.split(separator))
+    lista.pop(0)
+    duration = []
+    for j in lista:
+        duration.append(int(j[3]))
+    p = len(duration)
+    for d in range(0, p):
+        yield(duration[d])
+
+
+x = input()
+y = input()
+print(sum(wlasne_csv(x,y)))
